fed777e147732e0385105c0b37d9a3353df1fa49
masaya <masaya.kanbara@skym.co.jp>
2026-06-05 12:07:37 +0900

    update1.01

--- script.js
+++ script.js
@@ -30,23 +30,11 @@ function getRandomItem(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
 
-function shuffleArray(array) {
-  const shuffled = [...array];
-
-  for (let index = shuffled.length - 1; index > 0; index -= 1) {
-    const randomIndex = Math.floor(Math.random() * (index + 1));
-    [shuffled[index], shuffled[randomIndex]] = [shuffled[randomIndex], shuffled[index]];
-  }
-
-  return shuffled;
-}
-
 function getRandomToppings() {
   const toppingCount =
     Math.floor(Math.random() * (maxToppingCount - minToppingCount + 1)) + minToppingCount;
-  const shuffled = shuffleArray(toppingOptions);
 
-  return shuffled.slice(0, toppingCount);
+  return Array.from({ length: toppingCount }, () => getRandomItem(toppingOptions));
 }
 
 function generateMenu() {
